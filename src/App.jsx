36d65a6fe17008b9f39a5993e06a588aec2dfc27
S-Anbu <anbarasans750@gmail.com>
2024-09-18 17:35:00 +0530

    card gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { Cards } from './components/Cards'
+import { CarouselSlide } from './components/CarouselSlide'
 import { NavbarWithSolidBackground } from './components/NavbarWithSolidBackground'
 
 function App() {
@@ -8,7 +9,10 @@ function App() {
     <>
       <div className=''>
         <NavbarWithSolidBackground />
-        <div className='mt-5 px-2 grid gap-y-2  md:grid-cols-2 lg:grid-cols-3'>
+        <div className='mt-12 px-5'>
+        <CarouselSlide/>
+        </div>
+        <div className='mt-2 px-2 grid gap-y-2  md:grid-cols-2 lg:grid-cols-3'>
         <Cards/>
         <Cards/>
         <Cards/>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -14,7 +14,7 @@ import {
           <img
             src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
             alt="card-image"
-            className="w-full h-full"
+            className=""
           />
         </CardHeader>
         <CardBody>
